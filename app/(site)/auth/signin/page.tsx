336fe6e83582f3487d32d8fde97d34438ea66a0c
Roberth Oliveira <roberth.oliveira52@gmail.com>
2025-02-27 12:32:34 -0300

    fix names

--- app/(site)/auth/signin/page.tsx
+++ app/(site)/auth/signin/page.tsx
@@ -2,10 +2,7 @@ import Signin from "@/components/Auth/Signin";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Login Page - Solid SaaS Boilerplate",
-
-  // other metadata
-  description: "This is Login page for Startup Pro"
+  title: "Entre na sua conta - Unindo Destinos",
 };
 
 const SigninPage = () => {
